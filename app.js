959e9dda264e6cab35554299c1397290529d7d89
minahilh21 <minahilh21@gmail.com>
2022-06-21 20:00:43 +0500

    updated package

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ app.set("view engine","jade")
 app.use(express.static(path.join(__dirname, "/assets")));
 
 app.get('/', function (req, res) {
-    var stdList = [111,2222,3333];
-    res.render('index',{studentList:stdList });
+    res.render('index');
 });
 
 var server = app.listen(3100, function () {
