d4ceff319db5fa7c598a0274d5e4384098ce32de
minahilh21 <minahilh21@gmail.com>
2022-06-25 20:36:18 +0500

    added send email function

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const bodyParser = require('body-parser');
+const nodemailer = require("nodemailer");
+
 app.set("view engine","jade")
 app.use(express.static(path.join(__dirname, "/assets")));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
     res.render('index');
@@ -10,7 +16,35 @@ app.get('/', function (req, res) {
 app.get('/cv', function (req, res) {
     res.sendFile(__dirname + "/Hassam-Resume.pdf");
 });
+app.post("/send_email",function(req,res,next){
+    sendEmail(req.body);
+    res.redirect("/");
+});
 
 var server = app.listen(3100, function () {
     console.log('http://localhost:3100/');
 });
+
+async function sendEmail(emailInfo) {
+  let testAccount = await nodemailer.createTestAccount();
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: emailInfo.email,
+    to: "minahilh21@gmail.com",
+    subject: emailInfo.subject,
+    text: emailInfo.message,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
